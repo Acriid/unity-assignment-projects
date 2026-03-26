555dd49e0b4c073bf404c91be4c28d0269bc5a60
Acriid <wilrichgerdus@gmail.com>
2026-03-27 00:45:50 +0200

    Baked navmesh

---
+++ Eyesore/Assets/Scripts/Enemy/Tutorial/EnemyStartTrigger.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyStartTrigger : MonoBehaviour
+{
+    [SerializeField] private LightEnemy _enemy;
+    [SerializeField] private Vector2 _destination;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            _enemy.SetNavAgentDestination(_destination);
+        }
+    }
+}
--- Eyesore/Assets/Scripts/Enemy/Tutorial/LightEnemy.cs
+++ Eyesore/Assets/Scripts/Enemy/Tutorial/LightEnemy.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LightEnemy : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent _navAgent;
     
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") || collision.CompareTag("Player Light"))
+        {
+            
+        }
+    }
+
+    private void ResetEncounter()
+    {
+        
+    }
+    public void SetNavAgentDestination(Vector2 destination)
+    {
+        _navAgent.SetDestination(destination);
+    }
 }
