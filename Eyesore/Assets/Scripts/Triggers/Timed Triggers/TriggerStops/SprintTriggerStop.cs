95fab375b0e4b8c82f81ee564702af1fbbb3caf4
Acriid <wilrichgerdus@gmail.com>
2026-04-01 10:20:53 +0200

    Finishing touches for prototype 2

--- Eyesore/Assets/Scripts/Triggers/Timed Triggers/TriggerStops/SprintTriggerStop.cs
+++ Eyesore/Assets/Scripts/Triggers/Timed Triggers/TriggerStops/SprintTriggerStop.cs
@@ -5,6 +5,7 @@ public class SprintTriggerStop : MonoBehaviour
     [SerializeField] private TimedTrigger _timedTrigger;
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.CompareTag("Player"))
         _timedTrigger.StopTimer();
     }
 }
