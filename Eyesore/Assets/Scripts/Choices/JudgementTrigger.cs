b07e6da775a980c8ad65040e21203f1b8cf558d4
Acriid <wilrichgerdus@gmail.com>
2026-05-27 00:17:06 +0200

    Need to finish tutorial

--- Eyesore/Assets/Scripts/Choices/JudgementTrigger.cs
+++ Eyesore/Assets/Scripts/Choices/JudgementTrigger.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 
 public class JudgementTrigger : MonoBehaviour
 {
-    public GameObject mainCamera;
-    public GameObject EndTrigger;
+    public GameObject _endCanvas;
     
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            mainCamera.transform.position = new Vector3(-92f,-30f,-1f);
-            EndTrigger.transform.position = new Vector3(-110.5f,-30.5f,0);
-            EndTrigger.GetComponent<GameEnd>()._endCanvas
-            .GetComponentInChildren<EndDialogManager>().ChangeFollow(false);
+            _endCanvas.GetComponent<EndDialogManager>().ChangeFollow(false);
         }
     }
 }
